e0d6bcdb1d50e5919c15004c78a713681dab662d
mohan_kalahasthi <mohan@anyware-esolutions.com>
2020-01-31 09:12:10 +0530

    commit create account

--- src/app/modules/auth/signup-details-page/signup-details-page.component.ts
+++ src/app/modules/auth/signup-details-page/signup-details-page.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FnParam } from '@angular/compiler/src/output/output_ast';
 
 @Component({
-  selector: 'app-signup-details-page',
-  templateUrl: './signup-details-page.component.html',
-  styleUrls: ['./signup-details-page.component.scss']
+  selector: "app-signup-details-page",
+  templateUrl: "./signup-details-page.component.html",
+  styleUrls: ["./signup-details-page.component.scss"]
 })
 export class SignupDetailsPageComponent implements OnInit {
-  email: any;
-  routeSubscription: any;
-  constructor(private activatedRoute:ActivatedRoute) { }
+  signupForm: FormGroup;
+
+  constructor() {}
 
   ngOnInit() {
-    this.routeSubscription = this.activatedRoute.params.subscribe((value: any) => {
-this.email = value.id;
-    })
-    this.serviceCall();
+    this.signupForm = new FormGroup({
+        username: new FormControl(null, [Validators.required]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [Validators.required]),
+        confirmPassword:new FormControl(null,[Validators.required]),
+        displayName: new FormControl(null,[Validators.required]),
+        firstName: new FormControl(null,[Validators.required]),
+        lastName: new FormControl(null,[Validators.required]),
+        nickName: new FormControl(null,[Validators.required]),
+        website: new FormControl(null,[Validators.required]),
+        bioDescription: new FormControl(null,[Validators.required]),
+        jabber: new FormControl(null,[Validators.required]),
+        aolIm: new FormControl(null,[Validators.required]),
+        yahooIm: new FormControl(null,[Validators.required]),
+        
+    });
   }
 
-
-  serviceCall(){
-console.log(this.routeSubscription)
+  submit() {
+    console.log(this.signupForm);
   }
-
 }
